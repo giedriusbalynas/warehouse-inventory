2114968b2a14708bfe7183455ce9575b3af494b4
giedriusbalynas <balynas.giedrius@gmail.com>
2020-02-27 17:30:33 +0200

    Create product, navbar, beginning of list.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+// import NewProduct from './containers/NewProduct/NewProduct'
+import Layout from "./hoc/Layout/Layout";
+import List from './containers/List/List'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
-      </div>
+      <React.Fragment>
+          <Navbar />
+          <Layout>
+              <List />
+              {/*<NewProduct />*/}
+          </Layout>
+      </React.Fragment>
     );
   }
 }
